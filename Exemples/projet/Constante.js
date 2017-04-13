39e2b8b8f60de9a1207e78587dcbb0553c5bee1a
nakata532 <tangnakata@gmail.com>
2017-04-13 13:05:58 +0200

    implemation des constante

---
+++ Exemples/projet/Constante.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Frederic on 13/04/2017.
+ */
+
+
+
+const DEPT=["INFO","MMI","GEA","TC"];
+const UEINFO=["ue41","ue42","ue31","ue32","ue33","ue21","ue22","ue11","ue12"];
+
+
+// dictionnaire clé plus valeurs
+const SEMESTREINFO=[["S4",["ue41","ue42"]],["S3",["ue31","ue32","ue33"]],["S2",["ue21","ue22"]],["S1",["ue11","ue12"]]];
+
+const INFO_ue41=["Pweb2", "AdmSR", "ProgMobile", "ProgRep", "CompInfo", "Projet"];
+const INFO_ue42=["Atelier", "RO", "COM", "Ang"];
