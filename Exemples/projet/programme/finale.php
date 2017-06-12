cbd7c9473118ee1ea3985a43bcce59e85fb17a4f
nakata532 <tangnakata@gmail.com>
2017-06-12 15:59:31 +0200

    chercher le bon dossier sur tout les année precedent 	modified:   Exemples/projet/programme/finale.php

--- Exemples/projet/programme/finale.php
+++ Exemples/projet/programme/finale.php
@@ -10,39 +10,55 @@
 
 function salut($dept,$semestre,$annee,$groupe=null ){
 
-    if($semestre[1]>4 ) {
+    if($semestre[1]>4 || $semestre[1]<0) {
         error_log("erreur de semestre");
         exit(1);
     }
 
-    if ($semestre[1]==4) {
+    if ($semestre[1]>2) {
         for ($cptSemestre = $semestre[1]; $cptSemestre > 2; $cptSemestre--) {
             echo $dept . "_S" . $cptSemestre . "_" . $annee . PHP_EOL;
-            var_dump(file_exists($dept . "_S" . $cptSemestre . "_" . $annee));
 
         }
         $anneeprecedent=$annee;
-         $anneeprecedent[(strlen($annee)/2)-1]=$anneeprecedent[(strlen($annee)/2)-1]-1;
-         $anneeprecedent[(strlen($annee))-1]=$anneeprecedent[(strlen($annee))-1]-1;
+        $anneeprecedent[(strlen($annee)/2)-1]=$anneeprecedent[(strlen($annee)/2)-1]-1;
+        $anneeprecedent[(strlen($annee))-1]=$anneeprecedent[(strlen($annee))-1]-1;
         for ($cptSemestre = $semestre[1]; $cptSemestre > 0; $cptSemestre--) {
-            if (file_exists($dept . "_S" . $cptSemestre . "_" . $anneeprecedent))
-                echo $dept . "_S" . $cptSemestre . "_" . $anneeprecedent . PHP_EOL;
+            echo $dept . "_S" . $cptSemestre . "_" . $anneeprecedent . PHP_EOL;
         }
 
+        $anneeprecedent[(strlen($annee)/2)-1]=$anneeprecedent[(strlen($annee)/2)-1]-1;
+        $anneeprecedent[(strlen($annee))-1]=$anneeprecedent[(strlen($annee))-1]-1;
+        for ($cptSemestre =2; $cptSemestre > 0; $cptSemestre--) {
+            echo $dept . "_S" . $cptSemestre . "_" . $anneeprecedent . PHP_EOL;
 
 
-
-
+        }
     }
+    else{
+
+        for ($cptSemestre = $semestre[1]; $cptSemestre > 0; $cptSemestre--) {
+            echo $dept . "_S" . $cptSemestre . "_" . $annee . PHP_EOL;
+
+        }
+        $anneeprecedent=$annee;
+        $anneeprecedent[(strlen($annee)/2)-1]=$anneeprecedent[(strlen($annee)/2)-1]-1;
+        $anneeprecedent[(strlen($annee))-1]=$anneeprecedent[(strlen($annee))-1]-1;
+        for ($cptSemestre = $semestre[1]; $cptSemestre > 0; $cptSemestre--) {
+            echo $dept . "_S" . $cptSemestre . "_" . $anneeprecedent . PHP_EOL;
+        }
+    }
+
+
 
 
 
 }
 if ( count($argv)==4){
-    salut($argv[1],$argv[2],$argv[3]);
+    salut($argv[1],$argv[2],$argv[3]);/// 1er argument : Departement , 2eme argument : Semestre , 3 eme argument: année
 }
 else
-    salut($argv[1],$argv[2],$argv[3],$argv[4]);
+    salut($argv[1],$argv[2],$argv[3],$argv[4]);// 4 eme argument : le groupe
 
 
 
