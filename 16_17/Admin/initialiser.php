6af9830c859533af0f1bd6c8db00ba7cad0b3a5d
Cornu Bastien <bastien_cornu@ens.univ-artois.fr>
2017-10-24 14:13:05 +0200

    feat (traitement): Récuperation des données CVS Etudiant et Matière dans des tableaux.
    Les fonctions readCSVEtudiant() et readCSVMatieres() permettent de récupèrer toutes les données du fichier dans différents tableaux afin de les utliser dans le but
    de créer mes fichiers xls.

--- 16_17/Admin/initialiser.php
+++ 16_17/Admin/initialiser.php
@@ -1,26 +1,34 @@
 <?php
-
+//tableau pour stocker les données des fichiers CSV
+$infoEtu = [];
+$info_person = [];
+$infoMat = [];
+$info_matiere = [];
+$ue = [];
+$departement = [];
 
 //Si il n'y a pas de paramtre ou plus de 1, on arrête le programme est on déclenche une erreur.
-if($argc != 2){
-	echo "Erreur paramètre : passer le semestre choisi ".PHP_EOL;
-	exit(1);
+if ($argc != 2) {
+    echo "Erreur paramètre : passer le semestre choisi " . PHP_EOL;
+    exit(1);
 }
 
 //Vérifie si le parametre est un semestre sinon il declenche une erreur.
-function checkParam($semestre){
-	if(strcmp($semestre,"S1") == 0|| strcmp($semestre,"S2") == 0 || strcmp($semestre,"S3") == 0|| strcmp($semestre,"S4") == 0){
-		return;
-	}
-	echo "Erreur : Le paramètre n'est pas un semestre (S1,S2,S3,S4)".PHP_EOL;
+function checkParam($semestre)
+{
+    if (strcmp($semestre, "S1") == 0 || strcmp($semestre, "S2") == 0 || strcmp($semestre, "S3") == 0 || strcmp($semestre, "S4") == 0) {
+        return;
+    }
+    echo "Erreur : Le paramètre n'est pas un semestre (S1,S2,S3,S4)" . PHP_EOL;
+    exit(1);
 }
 
 //Permet de verifier si le chemin vers ../<semestre>/<xls> existe, est si il existe il affiche l'interieur du repertoire.
-function openDirectories($repertoire,$destination)
+function openDirectories($repertoire, $destination)
 {
     checkParam($repertoire);
-    $dir = "../" . $repertoire."/".$destination."/";
-    echo $dir.PHP_EOL;
+    $dir = "../" . $repertoire . "/" . $destination . "/";
+    echo $dir . PHP_EOL;
 
     // Ouvre un dossier bien connu, et liste tous les fichiers
     if (is_dir($dir)) {
@@ -30,22 +38,24 @@ function openDirectories($repertoire,$destination)
             }
             closedir($dh);
         }
+    } else {
+        echo "Erreur : Le dossier n'existe pas";
     }
     return $dir;
 }
 
 //Récuperer les fichier csv dans le repertoire Admin/
-function recoveryfileCSV(){
+function recoveryfileCSV()
+{
     $fileCSV = [];
     $chemin = ".";
     $fileCSV[] = $chemin;
     $dir = opendir($chemin);
     //Tant qu'il y a des fichier dans dir, on continue.
-    while($file = readdir($dir)) {
-        //si le fichier contient "csv" quelque part dans la string alors il entre dans le if
-        if(stripos($file, ".csv") !== false ){
-           echo $file.PHP_EOL;
-           $fileCSV[] = $file;
+    while ($file = readdir($dir)) {
+        //si le fichier contient "csv" quelque part dans la string alors il entre dans la condition
+        if (stripos($file, "csv") !== false) {
+            $fileCSV[] = $file;
 
         }
     }
@@ -53,34 +63,60 @@ function recoveryfileCSV(){
     return $fileCSV;
 }
 
-function readCSV(){
+//Ecriture dans infoEtu: Numéro,Nom,... puis dans info_person: les elements de chaque personne
+function readCSVEtudiant()
+{
+    global $infoEtu, $info_person;
     $tab = recoveryfileCSV();
-    $info = [];
-    $infoperson = [];
     $compteur = 0;
     if (($handle = fopen($tab[1], "r")) !== FALSE) {
         while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
             $num = count($data);
-            for ($c=0; $c < $num; $c++) {
-                if($compteur == 0){
-                    $info[] = $data[$c];
+            for ($c = 0; $c < $num; $c++) {
+                if ($compteur == 0) {
+                    $infoEtu[] = $data[$c];
                 } else {
-                    $infoperson[$compteur] =$info[$c]."=>".$data[$c];
+                    $info_person[$compteur][$infoEtu[$c]] = $data[$c];
                 }
             }
             $compteur++;
         }
         fclose($handle);
     }
-    echo "********** Information **********".PHP_EOL;
-    foreach ($info as $value){
-        echo $value.PHP_EOL;
-    }
-    echo "********** Information Personne **********".PHP_EOL;
-    foreach ($infoperson as $key=>$value){
-        echo $key.")"." ".$value.PHP_EOL;
+}
+
+//Ecriture dans infoMat: Référence,Nom Module,... puis dans info_matiere: les elements de chaque matiere
+function readCSVMatieres()
+{
+    global $infoMat, $info_matiere, $ue, $departement;
+    $tab = recoveryfileCSV();
+    $compteur = 0;
+    if (($handle = fopen($tab[2], "r")) !== FALSE) {
+        while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
+            $num = count($data);
+            for ($c = 0; $c < $num; $c++) {
+                if ($compteur == 0) {
+                    $infoMat[] = $data[$c];
+                } else {
+                    if($data[0] == ""){
+                        $compteur++;
+                        break;
+                    } elseif (stripos($data[0],"UE") !== false){
+                        $ue[$data[0]][] = $data[$c];
+                    } elseif (stripos($data[0],"M3") !== false ) {
+                        $info_matiere[$compteur][$infoMat[$c]] = $data[$c];
+                    } else{
+                        $departement[$data[0]][] = $data[$c];
+                    }
+                }
+            }
+            $compteur++;
+        }
+        fclose($handle);
     }
 }
 
-openDirectories($argv[1],"xls");
-readCSV();
+openDirectories($argv[1], "xls");
+$csv = recoveryfileCSV();
+readCSVEtudiant();
+readCSVMatieres();
